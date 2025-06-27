552ac027a4d407619bd442ca09a4310361e0e9d4
mllrosa <mllr8sa@gmail.com>
2025-06-27 16:32:27 +0000

    m

--- requests/requests.py
+++ requests/requests.py
@@ -1,4 +1,22 @@
+"""
 Pegar Informações - GET
 Criar Informações - POST
 Editar Informações - PATCH
 Deletar Informações - DELETE
+"""
+
+import requests
+requisicao = requests.get("")
+print(requisicao) #resposta 200
+print(requisicao.json()) #pegar a informação armazenada dentro da requisição
+
+"""
+INFORMAÇÃO NO FORMATO JSON
+{
+  "nome": "João",
+  "idade": 30,
+  "cidade": "São Paulo"
+}
+
+"""
+
