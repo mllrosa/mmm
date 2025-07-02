80ea14e69e4f35ae326e68d0f88697553ae25ff7
mllrosa <mllr8sa@gmail.com>
2025-07-02 09:16:51 -0300

    Atualizar o requests.py

--- requests/requests.py
+++ requests/requests.py
@@ -4,11 +4,46 @@ Criar Informações - POST
 Editar Informações - PATCH
 Deletar Informações - DELETE
 """
-
+#GET
 import requests
 requisicao = requests.get("")
 print(requisicao) #resposta 200
 print(requisicao.json()) #pegar a informação armazenada dentro da requisição
+
+#POST
+import requests
+url = ""
+dados = {"nome": "Rosa", "idade": 17}
+requisicao = requests.post(url, json=dados)
+print(requisicao) # resposta 200
+print(requisicao.json()) # resposta do servidor
+
+#PUT/PATCH
+#atualiza tudo put
+import requests
+id = 3
+url = f"ejjshsvjs/{id}"
+dados = {"nome": "Rosaa Atualizada", "idade": 18}
+requisicao = requests.put(url, json=dados)
+print(requisicao)             
+print(requisicao.json())
+
+#atualiza parcialmente patch
+import requests
+id = 1
+url = f"ejjshsvjs/{id}"
+dados = {"idade": 19}  # Só atualiza a idade
+requisicao = requests.patch(url, json=dados)
+print(requisicao)
+print(requisicao.json())
+
+#DELETE
+import requests
+id = 3
+url = f"ejjshsvjs/{id}"
+requisicao = requests.delete(url)
+print(requisicao)
+print(requisicao.status_code)
 
 """
 INFORMAÇÃO NO FORMATO JSON
